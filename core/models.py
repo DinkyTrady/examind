192fc4732f3ff90977b232dee18502c59365fbd8
DinkyTrady <DinkyTrady@kyra.com>
2025-05-28 20:14:03 +0700

    feat: Adding Siswa models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class Siswa(models.Model):
+    id = models.AutoField(primary_key=True)
+    nama = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    nis = models.CharField(max_length=20, unique=True)
+    kelas = models.CharField(max_length=10)

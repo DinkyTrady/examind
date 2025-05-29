9c9db6e4c4f16abc6743dff220505d993b7d7b2d
Rendyprobe <rendy221205@gmail.com>
2025-05-29 14:07:52 +0700

    Halaman Soal Ujian Siswa

--- core/urls.py
+++ core/urls.py
@@ -14,5 +14,14 @@ urlpatterns = [
         auth_views.LogoutView.as_view(template_name="logout.html"),
         name="logout",
     ),
-    path('dash_siswa/', views.dash_siswa, name='dash_siswa'),
-]
+    
+    path('dash_siswa/', views.dash_siswa, name='siswa/dash_siswa.html'),
+    path('exam/<int:exam_id>/', views.exam_view, name='exam_view'),
+    path('save_answer/', views.save_answer, name='save_answer'),
+    path('submit_exam/', views.submit_exam, name='submit_exam'),
+    path('exam_result/<int:exam_id>/', views.exam_result, name='exam_result'),
+    path('mark_review/', views.mark_review, name='mark_review'),
+]   
+
+
+    
